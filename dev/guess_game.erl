79b174478c91700d4e8901dcdda161992cca0245
Raj-IT-Geek <rajendra.kantipudi@gmail.com>
2018-01-07 22:01:54 +0530

    modified guess_game to single user mode.
    modified toll to enhance logic as per Chaitanya's Suggestions.

--- dev/guess_game.erl
+++ dev/guess_game.erl
@@ -1,19 +1,12 @@
 -module(guess_game).
--export([start_server/0, stop_server/0, new_game/1, my_guess/1, server_loop/0, game_loop/1]).
+-export([start/1, my_guess/1, loop/1]).
 
-start_server() ->
-	register(game_server, spawn(?MODULE, server_loop, [])).
-
-stop_server() ->
-    game_server ! {self(), stop},
-	get_reply().
-
-new_game(Max_Range) ->
-	game_server ! {self(), new, Max_Range},
-	get_reply().
+start(Max_Range) ->
+	Random_Number = rand:uniform(Max_Range),
+	register(game_server, spawn(?MODULE, loop, [Random_Number])).
 
 my_guess(Guessed_Number) ->
-	current_game ! {self(), guess, Guessed_Number},
+	game_server ! {self(), guess, Guessed_Number},
 	get_reply().
 
 get_reply() ->
@@ -24,35 +17,16 @@ get_reply() ->
     end.
 
 
-server_loop() ->
+loop(Random_Number) ->
 	receive
-
-		{From, new, Max_Range} ->
-			Random_Number = rand:uniform(Max_Range),
-			%io:format("~n ~n Random Num... ~p~n~n", [Random_Number]),
-			register(current_game, spawn(?MODULE, game_loop, [Random_Number])),
-			From ! {ok, "Random Number Generated"},
-			server_loop();
-
-		{From, stop} ->
-			From ! "Game Server Stopped! ",
-			void
-
-	end.
-
-
-game_loop(Random_Number) ->
-	receive
-
 		{From, guess, Guessed_Number} when Guessed_Number == Random_Number ->
 			From ! "Hey you won!";
 
 		{From, guess, Guessed_Number} when Guessed_Number > Random_Number ->
 			From ! "Try a smaller number.",
-			game_loop(Random_Number);
+			loop(Random_Number);
 
 		{From, guess, Guessed_Number} when Guessed_Number < Random_Number ->
 			From ! "Try a bigger number. ",
-			game_loop(Random_Number)
-
+			loop(Random_Number)
 	end.
--- dev/toll.erl
+++ dev/toll.erl
@@ -2,9 +2,8 @@
 -export([start/0, stop/0, enter/1, leave/1, status/0, server_loop/0]).
 
 start() ->
-    register(toll_server, spawn(?MODULE, server_loop, [])),
-	toll_server ! {self(), create_register},
-	get_response().
+    register(toll_server, spawn(?MODULE, server_loop, [])).
+	%get_response().
 
 stop() ->
     toll_server ! {self(), stop},
@@ -31,42 +30,40 @@ get_response() ->
     end.
 
 server_loop() ->
-	server_loop({}).
+	server_loop({men, 0, women, 0, children,0}).
 
-server_loop(Toll_Register) ->
+server_loop({men, X, women, Y, children, Z}) ->
     receive
 
-        {From, create_register} ->
-			Toll_Register_1 = {men, 0, women, 0, children, 0},
-			From ! {"Toll Server started & Register Initialized!", Toll_Register_1},
-            server_loop(Toll_Register_1);
+		{From, enter, man} ->
+            From ! ok,
+            server_loop({men, X+1, women, Y, children, Z});
+
+        {From, enter, woman} ->
+            From ! ok,
+            server_loop({men, X, women, Y+1, children, Z});
+
+        {From, enter, child} ->
+            From ! ok,
+            server_loop({men, X, women, Y, children, Z+1});
 
 
-		{From, enter, Human_type} ->
-			{men, X, women, Y, children, Z} = Toll_Register,
-			case Human_type of
-				man -> Toll_Register_1 = {men, X+1, women, Y, children, Z};
-				woman -> Toll_Register_1 = {men, X, women, Y+1, children, Z};
-				child -> Toll_Register_1 = {men, X, women, Y, children, Z+1}
-			end,
-			From ! {ok, Toll_Register_1},
-			server_loop(Toll_Register_1);
+        {From, leave, man} ->
+            From ! ok,
+            server_loop({men, X-1, women, Y, children, Z});
 
+        {From, leave, woman} ->
+            From ! ok,
+            server_loop({men, X, women, Y-1, children, Z});
 
-		{From, leave, Human_type} ->
-			{men, X, women, Y, children, Z} = Toll_Register,
-			case Human_type of
-				man -> Toll_Register_1 = {men, X-1, women, Y, children, Z};
-				woman -> Toll_Register_1 = {men, X, women, Y-1, children, Z};
-				child -> Toll_Register_1 = {men, X, women, Y, children, Z-1}
-			end,
-			From ! {ok, Toll_Register_1},
-			server_loop(Toll_Register_1);
+        {From, leave, child} ->
+            From ! ok,
+            server_loop({men, X, women, Y, children, Z-1});
 
 
         {From, current_status} ->
-            From ! {ok, Toll_Register},
-            server_loop(Toll_Register);
+            From ! {ok, {men, X, women, Y, children, Z}},
+            server_loop({men, X, women, Y, children, Z});
 
         {From, stop} ->
 			From ! "Toll Server Stopped! ",
